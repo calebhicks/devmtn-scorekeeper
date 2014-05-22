493e27fb8fefd85ef97cc8a685c3a386cc2d0285
Caleb Hicks <calebhicks@gmail.com>
2014-05-21 22:55:56 -0600

    first commit

---
+++ Score Keeper/SXViewController.h
@@ -0,0 +1,13 @@
+//
+//  SXViewController.h
+//  Score Keeper
+//
+//  Created by Caleb Hicks on 5/21/14.
+//  Copyright (c) 2014 Dev Mountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXViewController : UIViewController
+
+@end
